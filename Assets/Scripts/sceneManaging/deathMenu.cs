c50434437c0d40c30cef1292f134e9e5774694d1
ascodeasice <ab0988956087@gmail.com>
2022-12-08 19:46:52 +0800

    Fix bug of not resetting coin count

--- Assets/Scripts/sceneManaging/deathMenu.cs
+++ Assets/Scripts/sceneManaging/deathMenu.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class deathMenu : MonoBehaviour
 {
     public void backToMenu()
     {
+        collectItem.coinCount = 0;
         SceneManager.LoadScene("MenuScene");
     }
 }
